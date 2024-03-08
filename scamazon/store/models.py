679262e6bc487943aff0b1b38570ab2a23baef11
Reese Sorrell <joreesesorrell@gmail.com>
2024-03-07 22:03:00 -0600

    Add basic sign up, login, and routing

--- scamazon/store/models.py
+++ scamazon/store/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    type = models.CharField(max_length=10);
 
 class Book(models.Model):
     title = models.CharField(max_length=200);
--- scamazon/store/views.py
+++ scamazon/store/views.py
@@ -1,5 +1,58 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Book
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from .forms import SignupForm
+from django.contrib.auth.models import Group
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the store index.")
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('router')
+    else:
+        form = SignupForm()
+    return render(request, 'signup.html', {'form': form})
+
+@login_required
+def router(request):
+    num_books = Book.objects.all().count()
+
+    context = {
+        'num_books': num_books,
+        'user_type': request.user.type,
+    }
+
+    if (request.user.type == "Buyer"):
+        return redirect('buyer_dashboard')
+    
+    elif (request.user.type =="Seller"):
+        return redirect('seller_dashboard')
+
+    else:
+        return redirect('signup')
+    
+@login_required
+def buyer_dashboard(request):
+    num_books = Book.objects.all().count()
+
+    context = {
+        'num_books': num_books,
+        'user_type': request.user.type,
+    }
+
+    return render(request, 'buyer_dashboard.html', context=context)
+
+@login_required
+def seller_dashboard(request):
+    num_books = Book.objects.all().count()
+
+    context = {
+        'num_books': num_books,
+        'user_type': request.user.type,
+    }
+
+    return render(request, 'seller_dashboard.html', context=context)
