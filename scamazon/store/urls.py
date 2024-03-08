c4ad703f7a005319fc6b0dd974534b10e5b39d6d
Alexander Kellough <atk133@msstate.edu>
2024-03-08 14:39:02 -0600

    Added Author Browse Functionality, and Ordered Ouput

--- scamazon/store/urls.py
+++ scamazon/store/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path("accounts/signup/", views.signup, name="signup"),
     path("buyer/", views.buyer_dashboard, name="buyer_dashboard"),
     path("seller/", views.seller_dashboard, name="seller_dashboard"),
-    path("browse-books/", views.browse_books, name='browse-books')
+    path("browse-books/", views.browse_books, name='browse-books'),
+    path("browse-authors/", views.browse_authors, name='browse_authors'),
+    path("books/<str:isbn>/", views.book, name="book-view"),
+    path("author/<str:author>/", views.author, name="author-view"),
 ]
