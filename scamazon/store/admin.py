0f934019ecfd88e74295b8718fc089953961d88a
sskeen1 <155486172+sskeen1@users.noreply.github.com>
2024-03-08 15:31:02 -0600

    fixed admin/models location

--- scamazon/store/admin.py
+++ scamazon/store/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Cart, Listing
 
 admin.site.register(Book)
+admin.site.register(Cart)
+admin.site.register(Listing)
