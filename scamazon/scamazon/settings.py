c98f26367903ff6c806511d24ec0d13f1ba02c3f
Alexander Kellough <atk133@msstate.edu>
2024-03-20 20:52:06 -0500

    Added Image and Changed Database Tables

--- scamazon/scamazon/settings.py
+++ scamazon/scamazon/settings.py
@@ -129,3 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 
 AUTH_USER_MODEL = 'store.CustomUser'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- scamazon/store/urls.py
+++ scamazon/store/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -14,4 +16,4 @@ urlpatterns = [
     path("cart/", views.pull_cart, name="cart"),
     path("add_cart/<str:isbn>",views.add_cart, name="add_cart"),
     path("search/", views.search, name="search")
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
